39358132b120b9d2d455d29d64ba8a545ff30220
ocaviva <ana.morzin1@gmail.com>
2020-02-07 16:52:38 -0400

    Botao volta da tela de LoginPAge

--- lib/src/loginPage.dart
+++ lib/src/loginPage.dart
@@ -26,10 +26,10 @@ class _LoginPageState extends State<LoginPage> {
           children: <Widget>[
             Container(
               padding: EdgeInsets.only(left: 0, top: 10, bottom: 10),
-              child: Icon(Icons.keyboard_arrow_left, color: Colors.black),
+              child: Icon(Icons.arrow_back, size: 20.0 , color: Colors.white),
+              //child: Icon(Icons.keyboard_arrow_left, color: Colors.black),
             ),
-            Text('Back',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
+            _padraoTexto('Voltar', 14.0)
           ],
         ),
       ),
